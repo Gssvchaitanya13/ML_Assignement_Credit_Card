f4d46dc80ddef68ee36de2099215d7e5f3245a2d
Gssvchaitanya13 <ssvgssv13@gmail.com>
2022-07-11 19:51:17 +0530

    setup.py updated

--- setup.py
+++ setup.py
@@ -1,1 +1,20 @@
 from setuptools import setup
+PROJECT_NAME="credit_card_prediction"
+AUTHOR="gssv"
+VERSION="0.0.1"
+PACKAGES=["credit_card"]
+DESCRIPTION="This is Credit Card Project"
+REQUIREMENT_FILE_NAME="requirements.txt"
+
+def get_requirements_list():
+    with open(REQUIREMENT_FILE_NAME) as requirement_file:
+        return requirement_file.readlines()
+setup(
+    name=PROJECT_NAME,
+    author=AUTHOR,
+    version=VERSION,
+    description=DESCRIPTION,
+    packages=PACKAGES,
+    install_requires=get_requirements_list()
+
+)
